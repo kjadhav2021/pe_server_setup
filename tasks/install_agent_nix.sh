f8fd5407484b715bbc7e750b03810646ced0c622
kjadhav2021 <kjadhav102015@gmail.com>
2021-02-25 23:14:26 +0800

    Added new bolt tasks to install pe agent

---
+++ tasks/install_agent_nix.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -k https://puppet-ent-server.platform9.puppet.net:8140/packages/current/install.bash | sudo bash
--- tasks/install_pe_server.sh
+++ tasks/install_pe_server.sh
@@ -5,7 +5,7 @@ mkdir -p /home/centos/downloads
 
 cd /home/centos/downloads
 
-curl -JLo puppet-server.tar.gz 'https://pm.puppet.com/cgi-bin/download.cgi?dist=el&rel=8&arch=x86_64&ver=latest'
+curl -JLo puppet-server.tar.gz 'https://pm.puppet.com/cgi-bin/download.cgi?dist=el&rel=7&arch=x86_64&ver=latest'
 #curl -JLO 'https://pm.puppet.com/cgi-bin/download.cgi?dist=el&rel=8&arch=x86_64&ver=latest'
 
 tar -xf puppet-server.tar.gz
